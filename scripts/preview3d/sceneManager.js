e6f62528403814c8bca367f7da4f8aa783caaf2c
Pablo Velasquez <pevb97@gmail.com>
2026-05-08 13:56:15 -0500

    Corrige import de Three.js para preview 3D en navegador

--- scripts/preview3d/sceneManager.js
+++ scripts/preview3d/sceneManager.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js?module';
 
 export function createScene(canvas) {
   const scene = new THREE.Scene();
